5786bad66d769b4567acda05957ddac9e762372b
Michael Ngangom <michael.ngangom@gmail.com>
2017-08-21 22:36:50 +0200

    21_ListActivity

---
+++ EffectiveJava/app/src/main/java/effectivejava/effectivejava/CAndD.java
@@ -0,0 +1,10 @@
+package effectivejava.effectivejava;
+
+import android.app.ListActivity;
+
+/**
+ * Created by michaelngangom on 8/21/17.
+ */
+
+public class CAndD extends ListActivity {
+}
--- EffectiveJava/app/src/main/java/effectivejava/effectivejava/MainActivity.java
+++ EffectiveJava/app/src/main/java/effectivejava/effectivejava/MainActivity.java
@@ -1,7 +1,11 @@
 package effectivejava.effectivejava;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //crete an onItemClickListener
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView ListView, View v,int position,long id){
+                if(position ==0){
+                    Intent intent = new Intent(MainActivity.this,CAndD.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        //Add the lsitener to the lsit view
+        ListView listView = (ListView) findViewById(R.id.MainList);
+        listView.setOnItemClickListener(itemClickListener);
+
+
     }
     //make this a listView
 }
